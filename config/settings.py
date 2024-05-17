3949af0bc007405b74250ae053117f98b3e1fe01
angelstxrm <danilbelenkin07@gmail.com>
2024-05-17 15:54:48 +0300

    feat: added app

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += [
+    'store',
+]
+
+INSTALLED_APPS += [
     'rest_framework',
 ]
 
@@ -131,6 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
