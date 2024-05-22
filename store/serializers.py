26f274eb00515c495326b2e992ccb409f3df278d
angelstxrm <danilbelenkin07@gmail.com>
2024-05-22 12:37:00 +0300

    fix: deleted commits

--- store/serializers.py
+++ store/serializers.py
@@ -13,8 +13,4 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
 
-    # Валидация на цену.
-    # def validate_price(self, value):
-    #     if value < 0:
-    #         raise serializers.ValidationError('Цена не может быть меньше 0')
-    #     return value
+    
